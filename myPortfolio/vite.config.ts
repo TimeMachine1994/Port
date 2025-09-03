fca93d192bb0f754a3ed6a0f856621a400c2e211
TimeMachine1994 <austinbryanfilm@gmail.com>
2025-09-03 14:42:55 -0400

    check

--- myPortfolio/vite.config.ts
+++ myPortfolio/vite.config.ts
@@ -1,8 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), imagetools()]
+	plugins: [sveltekit(), imagetools()]
 });
